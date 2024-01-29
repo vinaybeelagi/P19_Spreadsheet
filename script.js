6d7ba287fb00eca70d686874e1d59c939001bf6d
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-29 18:22:36 +0530

    Object containing spreadsheet functions

--- script.js
+++ script.js
@@ -31,4 +31,21 @@ const infixToFunction = {
       : sorted[Math.ceil(middle)];
   }
   
- 
+  // Object containing spreadsheet functions
+  const spreadsheetFunctions = {
+    sum,
+    average,
+    median,
+    even: nums => nums.filter(isEven),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    firsttwo: nums => nums.slice(0, 2),
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: nums => range(...nums),
+    nodupes: nums => [...new Set(nums).values()]
+  };
+  
+  
