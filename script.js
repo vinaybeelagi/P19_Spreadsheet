252ebe452472e26f0a8d9ec7a9dcbede23c0504d
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-29 18:19:31 +0530

    Function to handle high precedence operations

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ const infixToFunction = {
   // Function to evaluate infix expressions using regular expressions
   const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
   
+  // Function to handle high precedence operations
+  const highPrecedence = str => {
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str, regex);
+    return str === str2 ? str : highPrecedence(str2);
+  }
+  
+  
