faa6cf837a260148d24239c91d0cbf3870374b20
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-29 18:18:01 +0530

    Function to evaluate infix expressions using regular expressions

---
+++ script.js
@@ -0,0 +1,11 @@
+// Define a map of operators to their corresponding functions
+const infixToFunction = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
+  }
+  
+  // Function to evaluate infix expressions using regular expressions
+  const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+  
