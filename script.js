786913246eb545753cd8a1707511e9b69568ac3c
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-29 18:20:43 +0530

    Utility functions for mathematical operations

--- script.js
+++ script.js
@@ -16,4 +16,19 @@ const infixToFunction = {
     return str === str2 ? str : highPrecedence(str2);
   }
   
+  // Utility functions for mathematical operations
+  const isEven = num => num % 2 === 0;
+  const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+  const average = nums => sum(nums) / nums.length;
   
+  // Function to calculate the median of an array
+  const median = nums => {
+    const sorted = nums.slice().sort((a, b) => a - b);
+    const length = sorted.length;
+    const middle = length / 2 - 1;
+    return isEven(length)
+      ? average([sorted[middle], sorted[middle + 1]])
+      : sorted[Math.ceil(middle)];
+  }
+  
+ 
